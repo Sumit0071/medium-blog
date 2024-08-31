2a53054d6d9ee750b158948b3164688ada6d3b23
[Sumit] <[sumitadhikari166@gmail.com]>
2024-08-31 09:48:46 +0530

    backend rest update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,14 @@ import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
 import { cors } from "hono/cors";
 const app = new Hono<{
-  Bindings: {
-    JWT_SECRET: string
-    DATABASE_URL: string;
-  }
+    Bindings: {
+        JWT_SECRET: string
+        DATABASE_URL: string;
+    }
 }>()
 app.use( '/*', cors() );
 app.get( '/', ( c ) => {
-  return c.text( "hello hono" );
+    return c.text( "hello hono" );
 } );
 app.route( "/api/v1/user", userRouter );
 app.route( "/api/v1/blog", blogRouter );
